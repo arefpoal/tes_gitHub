efb7c70710500980b881f0270a1340260d249b40
arefpoal <arefprasetyo@gmail.com>
2018-07-13 21:05:51 +0700

    tes GIT

---
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class FrontendController.
+ */
+class FrontendController extends Controller
+{
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('frontend.index');
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function macros()
+    {
+        return view('frontend.macros');
+    }
+	
+    public function tes()
+    {
+        return view('frontend.tes');
+    }
+}
